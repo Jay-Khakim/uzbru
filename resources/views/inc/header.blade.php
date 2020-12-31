098bf413b26956156a75fc7c8bca8a0d98ea25a0
Jay-Khakim <mgmediajay@gmail.com>
2020-12-31 19:01:15 +0500

    header localisation done

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -2,67 +2,67 @@
 <nav id="menu">
     <ul>
         <li class="active">
-            <a href="index.php">Asosiy</a>
+            <a href="index.php">@lang("Main")</a>
         </li>
-        <li><a href="com-history.php">Uyushma</a>
+        <li><a href="com-history.php">@lang("Association")</a>
             <ul >
 
-                <li><a href="com-history.php" ><span>Uyushma tarixi</span></a>
+                <li><a href="com-history.php" ><span>@lang("History")</span></a>
                 </li>
-                <li><a href="about.php" ><span>Biz haqimizda</span></a>
+                <li><a href="about.php" ><span>@lang("About Us")</span></a>
                 </li>
-                <li><a href="services.php" ><span>Asosiy xizmatlar</span></a>
+                <li><a href="services.php" ><span>@lang("Services")</span></a>
                 </li>
-                <li><a href="team.php" ><span>Rahbariyat va xodimlar</span></a>
-                <li><a href="branches.php" ><span>Filiallar</span></a>
+                <li><a href="team.php" ><span>@lang("Staff")</span></a>
+                <li><a href="branches.php" ><span>@lang("Branches")</span></a>
             </ul>
         </li>
         <li>
-            <a href="bemember.php">A'zolik </a>
+            <a href="bemember.php">@lang("Membership") </a>
             <ul >
-                <li ><a  href="bemember.php">A'zo bo'lish</a></li>
-                <li ><a  href="members.php">A'zolar ro'yhati</a></li>
-                <li ><a  href="checkmembers.php">A'zolikni tekshirish</a></li>
+                <li ><a  href="bemember.php">@lang("Become a member")</a></li>
+                <li ><a  href="members.php">@lang("List of members")</a></li>
+                <li ><a  href="checkmembers.php">@lang("Check for membership")</a></li>
             </ul>
         </li>
         <li>
-            <a href="comments.php">Ma'lumotlar</a>
+            <a href="comments.php">@lang("Information")</a>
             <ul >
-                <li><a href="comments.php">Fikr va mulohazalar</a></li>
-                <li><a href="partners.php">Hamkorlar</a></li>
-                <li><a href="faq.php">Savol va Javoblar</a></li>
+                <li><a href="comments.php">@lang("Feedbacks")</a></li>
+                <li><a href="partners.php">@lang("Partners")</a></li>
+                <li><a href="faq.php">@lang("FAQ")</a></li>
             </ul>
         </li>
         <li>
-            <a href="news.php">Yangiliklar</a>
+            <a href="news.php">@lang("News")</a>
             <ul >
-                                    <li><a href="news.php">Umumiy Yangiliklar</a></li>
-                                    <li ><a  href="news-2.php">Uyushma yangiliklari</a></li>
-                                </ul>
+                <li><a href="news.php">@lang("General News")</a></li>
+                <li ><a  href="news-2.php">@lang("Association News")</a></li>
+            </ul>
         </li>
         <li>
-            <a href="photo.php">Media</a>
+            <a href="photo.php">@lang("Media")</a>
             <ul >
-                <li ><a  href="photo.php">Rasmlar</a></li>
-                <li ><a  href="video.php">Videolar</a></li>
+                <li ><a  href="photo.php">@lang("Photos")</a></li>
+                <li ><a  href="video.php">@lang("Video")</a></li>
                 
             </ul>
         </li>
-        <li><a href="contact.php">Aloqa</a></li>
+        <li><a href="contact.php">@lang("Contacts")</a></li>
     </ul>
 </nav>
 <!-- /Mobile Menu -->
 <header class="header-3-fix" >
     <div class="nav-warp nav-warp-h3">
         <div class="navi-warp-home-3">
-            <a href="index.php" class="logo"><img src="images/Logo-on-dark.png" class="img-responsive" alt="Image"></a>
+            <a href="index.php" class="logo"><img src="/images/Logo-on-dark.png" class="img-responsive" alt="Image"></a>
             
             <div class="tb-social-lan language">
                 <a href="tel:+998712070098"><i class="fa fa-phone" aria-hidden="true"></i>+998(71)2070098</a>
                 <select class="lang">
-                    <option data-class="usa">English</option>
-                    <option data-class="uz">O'zbekcha</option>
-                    <option data-class="ru">Русский</option>
+                    <option data-class="usa">@lang("English")</option>
+                    <option data-class="uz">@lang("Uzbek")</option>
+                    <option data-class="ru">@lang("Russian")</option>
                 </select>
                 <ul>
                     <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
@@ -74,54 +74,54 @@
             <nav>
                         <ul class="navi-level-1 active-subcolor">
                             <li class="active">
-                                <a href="index.php">Asosiy</a>
+                                <a href="index.php">@lang("Main")</a>
                             </li>
-                            <li><a href="company_history.php">Uyushma</a>
+                            <li><a href="company_history.php">@lang("Association")</a>
                                 <ul class="navi-level-2">
 
-                                    <li><a href="com-history.php" ><span>Uyushma tarixi</span></a>
+                                    <li><a href="com-history.php" ><span>@lang("History")</span></a>
                                     </li>
-                                    <li><a href="about.php" ><span>Biz haqimizda</span></a>
+                                    <li><a href="about.php" ><span>@lang("About Us")</span></a>
                                     </li>
-                                    <li><a href="services.php" ><span>Asosiy xizmatlar</span></a>
+                                    <li><a href="services.php" ><span>@lang("Services")</span></a>
                                     </li>
-                                    <li><a href="team.php" ><span>Rahbariyat va xodimlar</span></a>
+                                    <li><a href="team.php" ><span>@lang("Staff")</span></a>
                                     </li>
-                                    <li><a href="branches.php" ><span>Filiallar</span></a>	
+                                    <li><a href="branches.php" ><span>@lang("Branches")</span></a>	
                                 </ul>
                             </li>
                             <li>
-                                <a href="bemember.php">A'zolik </a>
+                                <a href="bemember.php">@lang("Membership")</a>
                                 <ul class="navi-level-2">
-                                    <li ><a  href="bemember.php">A'zo bo'lish</a></li>
-                                    <li ><a  href="members.php">A'zolar ro'yhati</a></li>
-                                    <li ><a  href="checkmembers.php">A'zolikni tekshirish</a></li>
+                                    <li ><a  href="bemember.php">@lang("Become a member")</a></li>
+                                    <li ><a  href="members.php">@lang("List of members")</a></li>
+                                    <li ><a  href="checkmembers.php">@lang("Check for membership")</a></li>
                                 </ul>
                             </li>
                             <li>
-                                <a href="comments.php">Ma'lumotlar</a>
+                                <a href="comments.php">@lang("Information")</a>
                                 <ul class="navi-level-2">
-                                    <li><a href="comments.php">Fikr va mulohazalar</a></li>
-                                    <li><a href="partners.php">Hamkorlar</a></li>
-                                    <li><a href="faq.php">Savol va Javoblar</a></li>
+                                    <li><a href="comments.php">@lang("Feedbacks")</a></li>
+                                    <li><a href="partners.php">@lang("Partners")</a></li>
+                                    <li><a href="faq.php">@lang("FAQ")</a></li>
                                 </ul>
                             </li>
                             <li>
-                                <a href="news.php">Yangiliklar</a>
+                                <a href="news.php">@lang("News")</a>
                                 <ul class="navi-level-2">
-                                    <li><a href="news.php">Umumiy Yangiliklar</a></li>
-                                    <li ><a  href="news-2.php">Uyushma yangiliklari</a></li>
+                                    <li><a href="news.php">@lang("General News")</a></li>
+                                    <li ><a  href="news-2.php">@lang("Association News")</a></li>
                                 </ul>
                             </li>
                             <li>
-                                <a href="photo.php">Media</a>
+                                <a href="photo.php">@lang("Media")</a>
                                 <ul class="navi-level-2">
                                     
-                                    <li ><a  href="photo.php">Rasmlar</a></li>
-                                    <li ><a  href="video.php">Videolar</a></li>
+                                    <li ><a  href="photo.php">@lang("Photos")</a></li>
+                                    <li ><a  href="video.php">@lang("Video")</a></li>
                                 </ul>
                             </li>
-                            <li><a href="contact.php">Aloqa</a></li>
+                            <li><a href="contact.php">@lang("Contacts")</a></li>
                         </ul>
                     </nav>
             <a href="#menu" class="btn-menu-mobile"><i class="fa fa-bars" aria-hidden="true"></i></a>
