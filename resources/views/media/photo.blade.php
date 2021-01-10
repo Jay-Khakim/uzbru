f465b85f2affea00f96439a53bdea3b547ac43ae
Jay-Khakim <mgmediajay@gmail.com>
2021-01-10 14:53:41 +0500

    photos page language done

--- resources/views/media/photo.blade.php
+++ resources/views/media/photo.blade.php
@@ -2,6 +2,22 @@
 @section('title')
     <title>@lang("Photos") | O'zBRU</title>
 @endsection
+@section('language')
+    <li>
+        <a href="{{route('photos', 'en') }}"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+    </li>
+    <li>
+        <a href="{{route('photos', 'ru') }}"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+    </li>
+    <li>
+        <a href="{{route('photos', 'uz') }}"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+    </li>
+@endsection
+@section('mob-lang')
+<a href="{{route('photos', 'en') }}" style="color: white;"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+<a href="{{route('photos', 'ru') }}" style="color: white; padding-left: 15px;"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+<a href="{{route('photos', 'uz') }}" style="color: white; padding-left: 15px;"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+@endsection
 @section('content')
 <section class="no-padding sh-about">
     <div class="sub-header ">
