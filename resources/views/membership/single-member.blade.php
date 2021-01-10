05893d02d4774b51f6f1b3e4ea494c9b95b3bac1
Jay-Khakim <mgmediajay@gmail.com>
2021-01-10 15:21:15 +0500

    single member page language done

--- resources/views/membership/single-member.blade.php
+++ resources/views/membership/single-member.blade.php
@@ -3,6 +3,22 @@
 <title>{{$member->name}} | O'zBRU</title>
     
 @endsection
+@section('language')
+    <li>
+        <a href="{{route('single-member', ['language' => 'en', 'id' => $member->id])}}"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+    </li>
+    <li>
+        <a href="{{route('single-member', ['language' => 'ru', 'id' => $member->id])}}"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+    </li>
+    <li>
+        <a href="{{route('single-member', ['language' => 'uz', 'id' => $member->id])}}"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+    </li>
+@endsection
+@section('mob-lang')
+<a href="{{route('single-member', ['language' => 'en', 'id' => $member->id])}}" style="color: white;"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+<a href="{{route('single-member', ['language' => 'ru', 'id' => $member->id])}}" style="color: white; padding-left: 15px;"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+<a href="{{route('single-member', ['language' => 'uz', 'id' => $member->id])}}" style="color: white; padding-left: 15px;"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+@endsection
 
 @section('content')
 <section class="no-padding sh-case-single">
