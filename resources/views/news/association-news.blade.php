db9d0b89537615e557269feed86a3004852d1337
Jay-Khakim <mgmediajay@gmail.com>
2021-01-10 15:22:45 +0500

    association news page language done

--- resources/views/news/association-news.blade.php
+++ resources/views/news/association-news.blade.php
@@ -2,6 +2,22 @@
 @section('title')
 <title>@lang("Association News") | O'zBRU</title>
 @endsection
+@section('language')
+    <li>
+        <a href="{{route('association-news', 'en') }}"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+    </li>
+    <li>
+        <a href="{{route('association-news', 'ru') }}"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+    </li>
+    <li>
+        <a href="{{route('association-news', 'uz') }}"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+    </li>
+@endsection
+@section('mob-lang')
+<a href="{{route('association-news', 'en') }}" style="color: white;"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+<a href="{{route('association-news', 'ru') }}" style="color: white; padding-left: 15px;"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+<a href="{{route('association-news', 'uz') }}" style="color: white; padding-left: 15px;"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+@endsection
 @section('content')
 <section class="no-padding sh-news">
     <div class="sub-header ">
