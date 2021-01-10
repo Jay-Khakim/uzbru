037625b4ebe44b932f7cc53d99d7d2c8b2fd9829
Jay-Khakim <mgmediajay@gmail.com>
2021-01-10 14:59:10 +0500

    be member page language done

--- resources/views/membership/bemember.blade.php
+++ resources/views/membership/bemember.blade.php
@@ -3,6 +3,22 @@
     <title>@lang("Membership") | O'zBRU</title>
     
 @endsection
+@section('language')
+    <li>
+        <a href="{{route('bemember', 'en') }}"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+    </li>
+    <li>
+        <a href="{{route('bemember', 'ru') }}"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+    </li>
+    <li>
+        <a href="{{route('bemember', 'uz') }}"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+    </li>
+@endsection
+@section('mob-lang')
+<a href="{{route('bemember', 'en') }}" style="color: white;"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+<a href="{{route('bemember', 'ru') }}" style="color: white; padding-left: 15px;"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+<a href="{{route('bemember', 'uz') }}" style="color: white; padding-left: 15px;"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+@endsection
 @section('content')
 <section class="no-padding sh-company-history">
     <div class="sub-header ">
