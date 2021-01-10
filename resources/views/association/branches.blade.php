076d17eacc7b35c28da4715f10b3c3a5778ecbd0
Jay-Khakim <mgmediajay@gmail.com>
2021-01-10 13:57:54 +0500

    branches page language done

--- resources/views/association/branches.blade.php
+++ resources/views/association/branches.blade.php
@@ -2,6 +2,22 @@
 @section('title')
     <title>@lang("Branches") | O'zBRU</title> 
 @endsection
+@section('language')
+    <li>
+        <a href="{{route('branches', 'en') }}"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+    </li>
+    <li>
+        <a href="{{route('branches', 'ru') }}"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+    </li>
+    <li>
+        <a href="{{route('branches', 'uz') }}"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+    </li>
+@endsection
+@section('mob-lang')
+<a href="{{route('branches', 'en') }}" style="color: white;"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+<a href="{{route('branches', 'ru') }}" style="color: white; padding-left: 15px;"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+<a href="{{route('branches', 'uz') }}" style="color: white; padding-left: 15px;"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+@endsection
 @section('content')
 <section class="no-padding sh-contact">
     <div class="sub-header ">
