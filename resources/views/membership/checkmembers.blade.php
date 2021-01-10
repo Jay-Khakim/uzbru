bb1a91373857595c669d81eeca964b2a7292b26b
Jay-Khakim <mgmediajay@gmail.com>
2021-01-10 15:13:32 +0500

    chech member page language done

--- resources/views/membership/checkmembers.blade.php
+++ resources/views/membership/checkmembers.blade.php
@@ -2,6 +2,22 @@
 @section('title')
     <title>@lang("Check for membership") | O'zBRU</title>
 @endsection
+@section('language')
+    <li>
+        <a href="{{route('checkmembers', 'en') }}"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+    </li>
+    <li>
+        <a href="{{route('checkmembers', 'ru') }}"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+    </li>
+    <li>
+        <a href="{{route('checkmembers', 'uz') }}"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+    </li>
+@endsection
+@section('mob-lang')
+<a href="{{route('checkmembers', 'en') }}" style="color: white;"><img src="/images/icon/1.jpg" alt="JB's Language Icon"><span>En</span></a>
+<a href="{{route('checkmembers', 'ru') }}" style="color: white; padding-left: 15px;"><img src="/images/icon/2.jpg" alt="JB's Language Icon"><span>Ru</span></a>
+<a href="{{route('checkmembers', 'uz') }}" style="color: white; padding-left: 15px;"><img src="/images/icon/3.jpg" alt="JB's Language Icon"><span>Uz</span></a>
+@endsection
 @section('content')
 <section class="no-padding sh-company-history">
     <div class="sub-header ">
